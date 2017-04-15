3c72d1292d803956170f99bb65fcbd90d5b27edd
ShihaoJing <shihao.jing810@gmail.com>
2017-04-14 23:09:50 -0400

    init commit

---
+++ Channel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Shihao Jing on 4/14/17.
+//
+
+#include "Channel.h"
---
+++ Channel.h
@@ -0,0 +1,18 @@
+//
+// Created by Shihao Jing on 4/14/17.
+//
+
+#ifndef ENGINE_CHANNEL_H
+#define ENGINE_CHANNEL_H
+
+#include <vector>
+
+class Channel {
+public:
+private:
+    
+    std::vector<void*> mhandlers;
+};
+
+
+#endif //ENGINE_CHANNEL_H
