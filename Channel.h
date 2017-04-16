68b055864818dafd10f8dd5ad3ff990de3f5380f
ShihaoJing <shihao.jing810@gmail.com>
2017-04-15 22:48:58 -0400

    Add messaging

--- Channel.h
+++ Channel.h
@@ -2,16 +2,89 @@
 // Created by Shihao Jing on 4/14/17.
 //
 
-#ifndef ENGINE_CHANNEL_H
-#define ENGINE_CHANNEL_H
+#ifndef CHANNEL_H
+#define CHANNEL_H
 
 #include <vector>
+#include <algorithm>
+#include <functional>
+#include <mutex>
+#include <iostream>
 
 class Channel {
 public:
+  template<typename T>
+  class InternalChannel;
+
+  template<typename tMessage, typename tHandler>
+  static void add(tHandler *handler) {
+    // typically, the handler type is derived while the message type would be explicit
+    // e.g. Channel<MyEvent>::add(this);
+    InternalChannel<tMessage>::instance().add(handler); //forward to the appropriate queue
+  }
+
+  template<typename tMessage, typename tHandler>
+  static void remove(tHandler *handler) {
+    InternalChannel<tMessage>::instance().remove(handler);
+  }
+
+  template<typename tMessage>
+  static void broadcast(const tMessage &message) {
+    // usually no need to be explicit, the message type can be derived at compiletime
+    InternalChannel<tMessage>::instance().broadcast(message);
+  }
+
 private:
-    
-    std::vector<void*> mhandlers;
+  template<typename tMessage>
+  class InternalChannel {
+  public:
+    typedef std::function<void(const tMessage &)> Handler;
+
+    static InternalChannel &instance() {
+      static InternalChannel result;
+      return result;
+    }
+
+    template<typename tHandler>
+    void add(tHandler *handler) {
+      std::lock_guard<std::mutex> lock(mMutex);
+
+      mHandlers.push_back([handler](const tMessage &msg) { (*handler)(msg); });
+      mOriginalPtrs.push_back(handler);
+    }
+
+    template<typename tHandler>
+    void remove(tHandler *handler) {
+      std::lock_guard<std::mutex> lock(mMutex);
+
+      auto it = std::find(mOriginalPtrs.begin(), mOriginalPtrs.end(), handler);
+      if (it == mOriginalPtrs.end())
+        throw std::runtime_error("Tried to remove a handler that was not in the handler list");
+
+      auto idx = it - mOriginalPtrs.begin();
+
+      mHandlers.erase(mHandlers.begin() + idx);
+      mOriginalPtrs.erase(it);
+    }
+
+    void broadcast(const tMessage &msg) {
+      std::vector<Handler> localQueue(mHandlers.size());
+
+      {
+        std::lock_guard<std::mutex> lock(mMutex);
+        localQueue = mHandlers;
+      }
+
+      for (auto &handler : localQueue)
+        handler(msg);
+    }
+
+    private:
+      std::mutex mMutex;
+      std::vector<Handler> mHandlers;
+      std::vector<void *> mOriginalPtrs;
+    };
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "Channel.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    Channel channel;
     return 0;
 }
